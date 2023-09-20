1c6334c809da0759a81a7ff20a6245efbf94e3ed
Jen Nguyen <jenniferannnguyen@live.com>
2023-09-20 13:12:22 -0600

    Add imports for images that I hope to use

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,7 @@
 import './css/styles.css';
-
+import { hideForm } from './dom-updates'
+import '.images/hotelonmountain.png'
+import '.images/blurhotelonmountain.png'
 
 
 console.log('This is the JavaScript entry file - your code begins here.');
