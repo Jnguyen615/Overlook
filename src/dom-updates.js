510a0d3c23d842164f4287370f42c450db0647b9
Jen Nguyen <jenniferannnguyen@live.com>
2023-09-20 13:11:40 -0600

    Add setup for possbily hiding login page

--- src/dom-updates.js
+++ src/dom-updates.js
@@ -1,11 +1,7 @@
-const loginButton = document.getElementById('login-button')
+const form = document.getElementById("login-form");
+const loginButton = document.getElementById("login-button");
 
-loginButton.addEventListener('click', () => {
-  const form = document.getElementById('form')
-  if (form.style.display === 'none') {
-    form.style.display = 'block'
-  } else {
-    form.style.display = 'none'
-  }
-})
-
+loginButton.addEventListener("click", function () {
+  // Toggle the hidden attribute of the form
+  form.hidden = true
+});
