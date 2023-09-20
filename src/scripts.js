6b1c2c97d6d11c1e88fd65617324913107cdc147
Jen Nguyen <jenniferannnguyen@live.com>
2023-09-20 14:12:05 -0600

    Add window load event listener to pull in data and log it

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,18 @@
 import './css/styles.css';
-import { hideForm } from './dom-updates'
-import '.images/hotelonmountain.png'
-import '.images/blurhotelonmountain.png'
+import { promises } from './api-calls'
 
+document.addEventListener("DOMContentLoaded", function () {
+  let data;
+
+  Promise.all(promises)
+    .then(results => {
+      data = {
+        customers: results['0'].customers,
+        rooms: results['1'].rooms,
+        bookings: results['2'].bookings
+      }
+      console.log(data)
+    })
+  })
 
 console.log('This is the JavaScript entry file - your code begins here.');
