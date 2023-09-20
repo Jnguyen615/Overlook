fa35fcbbb2c899a6f774ab86963e90947e1ba6f2
Jen Nguyen <jenniferannnguyen@live.com>
2023-09-20 15:20:36 -0600

    Add api call

--- src/api-calls.js
+++ src/api-calls.js
@@ -1,7 +1,15 @@
 export const fetchData = (dataType) => {
   return fetch(`http://localhost:3001/api/v1/${dataType}`)
-  .then(response => response.json()) 
-}
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`Get network response was not ok: ${response.status}`)
+    }
+    return response.json();
+  }) 
+  .catch((error) => {
+    console.error(`Error fetching ${dataType}: ${error}`);
+  });
+};
 
 export const promises = [
   fetchData('customers'),
