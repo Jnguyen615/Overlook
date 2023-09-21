df0d232bb241daef1e0261ebdecdc9f9ccab3b2e
Jen Nguyen <jenniferannnguyen@live.com>
2023-09-21 15:41:09 -0600

    Modify tests to reflect data change

--- test/user-test.js
+++ test/user-test.js
@@ -1,17 +1,18 @@
+const expect = chai.expect;
 import chai from 'chai';
-import { bookings, rooms } from '../src/data/testData';
+import { data } from '../src/data/testData';
 import {
   getPastBookingsData,
   getRoomNumbers,
   getRoomsByUserAndRoomNumber,
   calculateTotalRoomCost,
-} from '../src/model';
-const expect = chai.expect;
+  } 
+  from '../src/model';
 
 describe('Get past bookings', function () {
   it('should retrieve past bookings data', function () {
     const userId = 13;
-    const pastBookings = getPastBookingsData(userId);
+    const pastBookings = getPastBookingsData(userId, data);
 
     expect(pastBookings).to.deep.equal([
       {
@@ -31,14 +32,14 @@ describe('Get past bookings', function () {
 
   it('should return an empty array when the user has no past bookings', function () {
     const userId = 7; // Assuming there's no userID 6 in your data
-    const pastBookings = getPastBookingsData(userId);
+    const pastBookings = getPastBookingsData(userId, data);
 
     expect(pastBookings).to.deep.equal([]);
   });
 
   it('should retrieve room numbers from bookings data', function () {
     const userId = 13;
-    const pastBookings = getRoomNumbers(userId);
+    const pastBookings = getRoomNumbers(userId, data);
 
     expect(pastBookings).to.deep.equal([12, 1]);
   });
@@ -47,7 +48,7 @@ describe('Get past bookings', function () {
     const userId = 13;
     const roomNumber = 1;
 
-    const room = getRoomsByUserAndRoomNumber(userId, roomNumber);
+    const room = getRoomsByUserAndRoomNumber(userId, roomNumber, data);
     expect(room).to.deep.equal({
       number: 1,
       roomType: 'residential suite',
@@ -61,7 +62,7 @@ describe('Get past bookings', function () {
   it('should return null when no matching booking is found', function () {
     const userId = 9;
     const roomNumber = 1;
-    const room = getRoomsByUserAndRoomNumber(userId, roomNumber);
+    const room = getRoomsByUserAndRoomNumber(userId, roomNumber, data);
 
     expect(room).to.equal(null);
   });
@@ -69,7 +70,7 @@ describe('Get past bookings', function () {
   it('should return null when userID is not found in the bookings data', function () {
     const userId = 999;
     const roomNumber = 1;
-    const room = getRoomsByUserAndRoomNumber(userId, roomNumber);
+    const room = getRoomsByUserAndRoomNumber(userId, roomNumber, data);
 
     expect(room).to.equal(null);
   });
@@ -77,8 +78,31 @@ describe('Get past bookings', function () {
   it('should return null when roomNumber is not found in the bookings data', function () {
     const userId = 13;
     const roomNumber = 999;
-    const room = getRoomsByUserAndRoomNumber(userId, roomNumber);
+    const room = getRoomsByUserAndRoomNumber(userId, roomNumber, data);
 
     expect(room).to.equal(null);
+  });
+});
+
+describe('Calculate total room cost', function () {
+  it('should calculate the total cost of rooms for a user with bookings', function () {
+    const userId = 13;
+    const totalCost = calculateTotalRoomCost(userId, data);
+   
+    expect(totalCost).to.equal(530.49); // Adjust the delta as needed
+  });
+
+  it('should calculate the total cost as zero for a user with no bookings', function () {
+    const userId = 6;
+    const totalCost = calculateTotalRoomCost(userId, data);
+
+    expect(totalCost).to.equal(0);
+  });
+
+  it('should calculate the total cost as zero when userID is not found in the bookings data', function () {
+    const userId = 999;
+    const totalCost = calculateTotalRoomCost(userId, data);
+
+    expect(totalCost).to.equal(0);
   });
 });
