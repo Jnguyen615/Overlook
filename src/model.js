e812625e49de6fb5d4b019c2d006748837e1aac1
Jen Nguyen <jenniferannnguyen@live.com>
2023-09-21 15:40:28 -0600

    Modify function parameters to reflect data change

--- src/model.js
+++ src/model.js
@@ -1,35 +1,49 @@
-import { bookings, rooms } from '../src/data/testData'
+import { data, bookings } from '../src/data/testData'
 
-export function getPastBookingsData (userId)  {
-  const pastUserBookings = bookings.filter((booking) => {
+export function getPastBookingsData (userId, data)  {
+  const pastUserBookings = data.bookings.filter((booking) => {
     return booking.userID === userId
   })
   return pastUserBookings
 }
   
-export function getRoomNumbers (userId) {
-  return bookings.filter((booking) => {
+export function getRoomNumbers (userId, data) {
+  return data.bookings.filter((booking) => {
      return booking.userID === userId
-    }).map(booking => booking.roomNumber)
+    })
+    .map(booking => booking.roomNumber)
   }
      
-export function getRoomObjects (userId) {
-  const pastUserBookings = bookings.filter((booking) => {
+export function getRoomObjects (userId, data) {
+  const pastUserBookings = data.bookings.filter((booking) => {
     return booking.userID === userId
    })
    const mappedRooms = pastUserBookings.map(booking => booking.roomNumber)
  }
 
- export function getRoomsByUserAndRoomNumber(userId, roomNumber) {
-  const matchingBooking = bookings.find((booking) => {
+ export function getRoomsByUserAndRoomNumber(userId, roomNumber, data) {
+  const matchingBooking = data.bookings.find((booking) => {
     return booking.userID === userId && booking.roomNumber === roomNumber;
   });
   if (matchingBooking) {
-    const room = rooms.find((room) => {
+    const room = data.rooms.find((room) => {
       return room.number === matchingBooking.roomNumber;
     });
-    return room;
+    return room || null;
   }
   return null;
+}
+
+export function calculateTotalRoomCost(userId, data) {
+  const userBookings = data.bookings.filter((booking) => booking.userID === userId);
+  const totalCost = userBookings.reduce((totalCost, booking) => {
+    const room = data.rooms.find((r) => r.number === booking.roomNumber);
+    if (room) {
+      return totalCost + room.costPerNight;
+    }
+    return totalCost;
+  }, 0);
+
+  return totalCost;
 }
 
