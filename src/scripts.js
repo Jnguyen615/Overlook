fdb742315b1194e3b0583d5dac2946fa27fc09cb
Jen Nguyen <jenniferannnguyen@live.com>
2023-09-21 21:41:37 -0600

    Add eventListeners for login button and new booking button and worked on handle login

--- src/scripts.js
+++ src/scripts.js
@@ -1,43 +1,49 @@
-import './css/styles.css'
-import { promises } from './api-calls'
-import { populateRoomCardSection, createRoomCard } from './dom-updates'
-import flatpickr from "flatpickr"
+import './css/styles.css';
+import { promises } from './api-calls';
+import { submitLogin } from './users';
+import { populateRoomCardSection, createRoomCard } from './dom-updates';
+import flatpickr from 'flatpickr';
+import { data } from './data/testData';
 
-const form = document.getElementById("login-form");
-const loginButton = document.getElementById("login-button")
-const mainPageLogo = document.querySelector('.title')
-const pastBookingsArea = document.querySelector('.past-booking-cards')
-const upcomingBookingsArea = document.querySelector('.upcoming-bookings-cards')
-const mainPageView = document.querySelector('.main-view')
-const newBookingButton = document.querySelector('#new-booking-button')
-const topBar = document.querySelector('.main-page-view-top-bar')
-
+const usernameInput = document.querySelector('input[name="username"]');
+const passwordInput = document.querySelector('input[name="password"]');
+const form = document.querySelector('#login-form');
+const loginButton = document.getElementById('login-button');
+const mainPageLogo = document.querySelector('.title');
+const pastBookingsArea = document.querySelector('.past-booking-cards');
+const upcomingBookingsArea = document.querySelector('.upcoming-bookings-cards');
+const mainPageView = document.querySelector('.main-view');
+const newBookingButton = document.querySelector('#new-booking-button');
+const topBar = document.querySelector('.main-page-view-top-bar');
 
 document.addEventListener('DOMContentLoaded', function () {
   let data;
-  Promise.all(promises)
-  .then(results => {
+  Promise.all(promises).then(results => {
     data = {
       customers: results['0'].customers,
       rooms: results['1'].rooms,
-      bookings: results['2'].bookings
-    }
-    console.log(data)
-  })
-})
-
-loginButton.addEventListener("click", function () {
-  form.hidden = true
-  mainPageLogo.hidden = true
-  mainPageView.hidden = false
-  topBar.style.display = 'flex'
-   rooms.forEach(function (room) {
-     createRoomCard(room, 'past-bookings-section');
-     populateRoomCardSection(room, 'upcoming-bookings-section');
-
-   })
+      bookings: results['2'].bookings,
+    };
+    console.log(data);
+  });
 });
 
-newBookingButton.addEventListener('click', function() {
-  mainPageView.hidden = true
-})
+loginButton.addEventListener('click', function (event) {
+  event.preventDefault();
+  submitLogin();
+  form.hidden = true;
+  mainPageLogo.hidden = true;
+  mainPageView.hidden = false;
+  topBar.style.display = 'flex';
+  return data.rooms.forEach(function (room) {
+    createRoomCard(room, 'past-bookings-section');
+    populateRoomCardSection(room, 'past-bookings-section');
+  });
+});
+
+newBookingButton.addEventListener('click', function () {
+  mainPageView.hidden = true;
+});
+
+// form.addEventListener('submit', (event) => {
+// })
