061d7a2979466d2c75fdc92bf14741b2248cef5e
Jen Nguyen <jenniferannnguyen@live.com>
2023-09-22 20:43:17 -0600

    Fix a lot of broken code

--- src/scripts.js
+++ src/scripts.js
@@ -1,43 +1,59 @@
-import './css/styles.css'
-import { promises } from './api-calls'
-import { populateRoomCardSection, createRoomCard } from './dom-updates'
-import flatpickr from "flatpickr"
+import './css/styles.css';
+import { fetchData } from './api-calls';
+import { submitLogin } from './customers';
+import { populateRoomCardSection, createRoomCard } from './dom-updates';
+import { getUserId, getRoomNumber, getRoomsByUser } from './bookings';
+import flatpickr from 'flatpickr';
 
-const form = document.getElementById("login-form");
-const loginButton = document.getElementById("login-button")
-const mainPageLogo = document.querySelector('.title')
-const pastBookingsArea = document.querySelector('.past-booking-cards')
-const upcomingBookingsArea = document.querySelector('.upcoming-bookings-cards')
-const mainPageView = document.querySelector('.main-view')
-const newBookingButton = document.querySelector('#new-booking-button')
-const topBar = document.querySelector('.main-page-view-top-bar')
+const form = document.getElementById('login-form');
+const loginButton = document.getElementById('login-button');
+const mainPageLogo = document.querySelector('.title');
+const mainPageView = document.querySelector('.main-view');
+const newBookingButton = document.querySelector('#new-booking-button');
+const topBar = document.querySelector('.main-page-view-top-bar');
 
+let data;
 
 document.addEventListener('DOMContentLoaded', function () {
-  let data;
+  const promises = [
+    fetchData('customers'),
+    fetchData('rooms'),
+    fetchData('bookings'),
+  ];
+
   Promise.all(promises)
-  .then(results => {
-    data = {
-      customers: results['0'].customers,
-      rooms: results['1'].rooms,
-      bookings: results['2'].bookings
-    }
-    console.log(data)
-  })
-})
-
-loginButton.addEventListener("click", function () {
-  form.hidden = true
-  mainPageLogo.hidden = true
-  mainPageView.hidden = false
-  topBar.style.display = 'flex'
-   rooms.forEach(function (room) {
-     createRoomCard(room, 'past-bookings-section');
-     populateRoomCardSection(room, 'upcoming-bookings-section');
-
-   })
+    .then(results => {
+      data = {
+        customers: results[0].customers,
+        rooms: results[1].rooms,
+        bookings: results[2].bookings,
+      };
+      console.log(data);
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
 });
 
-newBookingButton.addEventListener('click', function() {
-  mainPageView.hidden = true
-})
+loginButton.addEventListener('click', function (event) {
+  event.preventDefault();
+  const usernameInput = document.querySelector('#username');
+  const passwordInput = document.querySelector('#password');
+  const signInOrError = document.querySelector('.sign-in-or-error-tag');
+
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+
+  submitLogin(data, username, password, signInOrError);
+  form.hidden = true;
+  mainPageLogo.hidden = true;
+  mainPageView.hidden = false;
+  topBar.style.display = 'flex';
+  const userID = getUserId(username);
+  const roomNumber = getRoomNumber(userID, data);
+  getRoomsByUser(userID, data);
+});
+
+newBookingButton.addEventListener('click', function () {
+  mainPageView.hidden = true;
+});
