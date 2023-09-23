c01d0897c000e76dc5d20d15bfc0ef0eed83241e
Jen Nguyen <jenniferannnguyen@live.com>
2023-09-22 20:41:26 -0600

    Setup for populateCards function

--- src/dom-updates.js
+++ src/dom-updates.js
@@ -1,17 +1,17 @@
-const pastBookingsArea = document.querySelector('.past-booking-cards');
-const upcomingBookingsArea = document.querySelector('.upcoming-bookings-cards');
-const mainPageLogo = document.querySelector('.title');
-import { data } from './data/testData';
+const pastBookingsArea = document.querySelector('.past-booking-cards')
+const upcomingBookingsArea = document.querySelector('.upcoming-bookings-cards')
+const mainPageLogo = document.querySelector('.title')
+import { data } from './data/testData'
 
 export function createRoomCard(room) {
-  const card = document.createElement('div');
-  card.classList.add('room-card');
+  const card = document.createElement('div')
+  card.classList.add('room-card')
 
-  const roomNumber = document.createElement('p');
-  roomNumber.textContent = `Room ${room.number}`;
+  const roomNumber = document.createElement('p')
+  roomNumber.textContent = `Room ${room.number}`
   card.appendChild(roomNumber);
 
-  const roomDetails = document.createElement('ul');
+  const roomDetails = document.createElement('ul')
   roomDetails.innerHTML = `
     <li>Room Type: ${room.roomType}</li>
     <li>Bidet: ${room.bidet ? 'Yes' : 'No'}</li>
@@ -19,17 +19,30 @@ export function createRoomCard(room) {
     <li>Number of Beds: ${room.numBeds}</li>
     <li>Cost Per Night: $${room.costPerNight}</li>
   `;
-  card.appendChild(roomDetails);
-
+  card.appendChild(roomDetails)
   return card;
 }
 
 export function populateRoomCardSection() {
-  const section = document.querySelector('.past-booking-cards');
-  if (section) {
-    data.rooms.forEach(room => {
-      const roomCard = createRoomCard(room);
-      section.appendChild(roomCard);
+  if (pastBookingsArea) {
+    const customerBookings = getBookingsByID(customerID, data)
+    pastBookingsArea.innerHTML = ''
+    customerBookings.forEach(booking => {
+      const room = data.rooms.find(room => room.number === booking.roomNumber)
+      if (room) {
+        const roomCard = createRoomCard(room);
+        pastBookingsArea.appendChild(roomCard);
+      }
     });
   }
 }
+
+// export function populateRoomCardSection(rooms, sectionId) {
+//   const section = document.getElementById(sectionId);
+//   if (section) {
+//     rooms.forEach((room) => {
+//       const roomCard = createRoomCard(room);
+//       section.appendChild(roomCard);
+//     });
+//   }
+// }
