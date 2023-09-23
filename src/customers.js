d949dfb43ff02744107f41fed4e856ce3b8e87a8
Jen Nguyen <jenniferannnguyen@live.com>
2023-09-22 20:42:39 -0600

    Rename model to customers

---
+++ src/customers.js
@@ -0,0 +1,23 @@
+import { getRoomsByUser } from './bookings';
+
+export function submitLogin(data, username, password, signInOrError) {
+  const ID = Number(username.slice(8));
+  const checkID = ID > 0 && username.startsWith('customer');
+  const checkPassword = password === 'overlook2021';
+  if (checkID && checkPassword) {
+    const customerName = getCustomer(ID, data);
+    document.querySelector('.hello').textContent = `Welcome ${customerName}`;
+  } else {
+    signInOrError.textContent = 'Incorrect Username or Password';
+  }
+}
+
+export function getCustomer(ID, data) {
+  const customer = data.customers.find(customer => customer.id === ID);
+  if (customer) {
+    getRoomsByUser(ID, data);
+    return customer.name;
+  } else {
+    return null;
+  }
+}
